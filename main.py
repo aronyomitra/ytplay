f2b013b92b2fef0550df6cdeb1b0aaa98dfee023
Aronyo Mitra <aronyomitra@gmail.com>
2021-08-03 19:30:13 +0530

    Improved code structure

--- main.py
+++ main.py
@@ -9,53 +9,39 @@ query = input("Enter youtube search query: ")
 query = query.replace(' ', '+')
 url = 'https://youtube.com/results?search_query=' + query
 regex = re.compile(r'watch\?v=(\S{11})')
+searchLen = 5
 
 resp = request.urlopen(url)
 respData = resp.read()
 srespData = str(respData)
+quickplay = False
 
-instaplay = False
-if ('!p' in query):
-    instaplay = True
-    firstvid = regex.search(srespData)
-    temp = "https://youtube.com/watch?v=" + firstvid[1]
-    p = pafy.new(temp, basic=False)
-    print ()
-    print (p.title)
-    print (p.author)
-    print (p.duration)
-    print ()
-    audioStreamURL = p.getbestaudio().url
-
-    player = vlc.MediaPlayer(audioStreamURL)
-    player.play()
-
-    time.sleep(2)
-    while player.is_playing():
-        time.sleep(1)
-    exit()
-
-videos = re.findall(r'watch\?v=(\S{11})', srespData)
+if '!p' in query:
+    quickplay = True
+    searchLen = 1
 
 pafylist = []
-
-for i in range (0, 5):
-    temp = "https://youtube.com/watch?v=" + videos[i]
+start = 0
+for i in range (0, searchLen):
+    match = regex.search(srespData, start)
+    temp = 'https://youtube.com/watch?v=' + match[1]
     p = pafy.new(temp, basic = False)
     pafylist.append(p)
+    start = match.end()
 
-print ()
-for i in range (0, len(pafylist)):
-    p = pafylist[i]
-    print (str(i+1) + ") " + p.title)
-    print (p.author)
-    print (p.duration)
+choice = 1
+if not quickplay:               
     print ()
+    for i in range (0, len(pafylist)):
+        p = pafylist[i]
+        print (str(i+1) + ") " + p.title)
+        print (p.author)
+        print (p.duration)
+        print ()
 
-choice = int(input("\nEnter selection: "))
+    choice = int(input("\nEnter selection: "))
+
 audioStreamURL = pafylist[choice-1].getbestaudio().url
-
-# print (audioStreamURL)
 
 player = vlc.MediaPlayer(audioStreamURL)
 player.play()
