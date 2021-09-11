8f64b5cca24e3d5338a1320e2acc26d23f3f86cd
Aronyo Mitra <aronyomitra@gmail.com>
2021-09-12 04:17:55 +0530

    Redesign flow into a neverending main loop

--- main.py
+++ main.py
@@ -4,103 +4,135 @@ import pafy
 import vlc
 import time
 
-query = input("Enter youtube search query: ")
-
-query = query.replace(' ', '+')
-url = 'https://youtube.com/results?search_query=' + query
-regex = re.compile(r'watch\?v=(\S{11})')
+regex1 = re.compile(r'watch\?v=(\S{11})')
+regex2 = re.compile(r'^!s(earch)?(( +\S+)+)$', re.IGNORECASE)
+regex3 = re.compile(r'^!p(lay)?(( +\S+)+)$', re.IGNORECASE)
+# regex4 = re.compile(r'^!p(lay)? ((https://)?(www.)?youtube.com/watch\?v=\S{11})$', re.IGNORECASE)
 searchLen = 5
 
-resp = request.urlopen(url)
-respData = resp.read()
-srespData = str(respData)
-quickplay = False
+vlcInstance = vlc.Instance("--verbose=-1", "--no-video")
+player = vlcInstance.media_player_new()
 
-if '!p' in query:
-    quickplay = True
-    searchLen = 1
+# Main loop
+while True:
+    command = input("\n>>")
+    command = command.strip()
 
-pafylist = []
-start = 0
-for i in range (0, searchLen):
-    match = regex.search(srespData, start)
-    temp = 'https://youtube.com/watch?v=' + match[1]
-    p = pafy.new(temp, basic = False)
-    pafylist.append(p)
-    start = match.end()
+    if command.lower() == 'quit' or command.lower() == 'q' or command.lower() == 'exit':
+        break
+    
+    x = regex2.match(command)
+    y = regex3.match(command)
+    
+    if x:
+        searchquery = x.group(2).strip().replace(' ', '+')
+        url = 'https://youtube.com/results?search_query=' + searchquery
+        resp = request.urlopen(url)
+        respData = resp.read()
+        srespData = str(respData)
 
-choice = 1
-if not quickplay:               
-    print ()
-    for i in range (0, len(pafylist)):
-        p = pafylist[i]
-        try:
-            print (str(i+1) + ") " + p.title)
-        except KeyError:
-            p._ydl_info['like_count'] = 0
-            p._ydl_info['dislike_count'] = 0
-           
-            p._have_basic = True
-            print (str(i+1) + ") " + p.title)
+        pafylist = []
+        start = 0
+        for i in range (0, searchLen):
+            match = regex1.search(srespData, start)
+            temp = 'https://youtube.com/watch?v=' + match[1]
+            p = pafy.new(temp, basic = False)
+            pafylist.append(p)
+            start = match.end()
+        
+        print()
+        for i in range (0, len(pafylist)):
+            p = pafylist[i]
+            try:
+                print (str(i+1) + ") " + p.title)
+            except KeyError:
+                p._have_basic = True
+                print (str(i+1) + ") " + p.title)
+            print (p.author)
+            print (p.duration)
+            print ()
+
+        choice = int(input("\nEnter selection: "))
+        p = pafylist[choice-1]
+        audioStreamURL = p.getbestaudio().url
+
+        print ()
+        print (p.title)
         print (p.author)
         print (p.duration)
+        print ("(https://youtube.com/watch?v=" + p.videoid + ")")
+
+        player.set_mrl(audioStreamURL)
+        player.play()
+    
+    elif y:
+        searchquery = y.group(2).strip().replace(' ', '+')
+        url = 'https://youtube.com/results?search_query=' + searchquery
+        
+        resp = request.urlopen(url)
+        respData = resp.read()
+        srespData = str(respData)
+        
+        match = regex1.search(srespData)
+        url = 'https://youtube.com/watch?v=' + match[1]
+        try:
+            p = pafy.new(url)
+        except KeyError:
+            p._have_basic = True
+            p = pafy.new(url)
+        
+        audioStreamURL = p.getbestaudio().url
+        
         print ()
+        print (p.title)
+        print (p.author)
+        print (p.duration)
+        print ("(https://youtube.com/watch?v=" + p.videoid + ")")
 
-    choice = int(input("\nEnter selection: "))
+        player.set_mrl(audioStreamURL)
+        player.play()
 
-try:
-    audioStreamURL = pafylist[choice-1].getbestaudio().url
-except KeyError:
-    pafylist[choice-1]._have_basic = True
-    audioStreamURL = pafylist[choice-1].getbestaudio().url
+    command = command.lower()
 
-
-player = vlc.MediaPlayer(audioStreamURL)
-player.play()
-
-time.sleep(2)
-print ()
-print (pafylist[choice-1].title)
-print (pafylist[choice-1].author)
-print (pafylist[choice-1].duration)
-print ("(https://youtube.com/watch?v=" + pafylist[choice-1].videoid + ")")
-while True:
-    comm = input ("\n>>")
-    if comm == "p":
-        if player.get_state() == vlc.State.Playing:
+    if command == 'pause' or command == 'p':
+        state = player.get_state()
+        if state == vlc.State.Playing:
             print ("Paused")
-        elif player.get_state() == vlc.State.Ended or player.get_state() == vlc.State.Stopped:
+        elif state == vlc.State.Ended or state == vlc.State.Stopped:
             print ("Track has ended")
         else:
             print ("Resumed")
         player.pause()
-    elif comm == 's':
+    
+    elif command == 'stop' or command == 's':
         player.stop()
         print ("Stopped")
-    elif comm == 'restart':
+
+    elif command == 'play':
+        player.play()
+        print ("Playing")
+
+    elif command == 'replay' or command == 'restart':
         print ("Restarting track")
         player.stop()
         player.play()
-    elif comm == 'r':
-        player.set_time(player.get_time() + 10000)
-    elif comm == 'l':
-        player.set_time(player.get_time() - 10000)
-    elif comm == 'q':
-        break
-    elif comm == 'v+':
+    
+    elif command == 'v+':
         player.audio_set_volume(player.audio_get_volume() + 10)
         time.sleep(1)
         print ("Volume: " + str(player.audio_get_volume()))
-    elif comm == 'v-':
+    elif command == 'v-':
         player.audio_set_volume(player.audio_get_volume() - 10)
         time.sleep(1)
         print ("Volume: " + str(player.audio_get_volume()))
-    elif comm == 'mute':
+    elif command == 'mute':
         player.audio_toggle_mute()
-    elif comm == 'status':
+
+    elif command == 'status':
         print(player.get_state())
-    elif comm == 'np':
-        print ("Now Playing: " + pafylist[choice-1].title)
+
+    elif command == 'np' or command == "nowplaying":
+        print ("Now Playing: " + p.title)
         pos = player.get_position()
         pos = int(pos*30 + 1)
         print ("[", end='')
@@ -112,5 +144,44 @@ while True:
         print ("]")
         if player.get_state() == vlc.State.Paused:
             print ("Paused")
-    else:
-        print ("Invalid command. Try '?' for help")
+    
+    elif 'fw' in command:
+        x = re.match(r'^fw( \d+)?$', command)
+        if x:
+            delta = 10
+            if x.group(1) is not None:
+                delta = int(x.group(1).strip())
+        
+            player.set_time(player.get_time() + delta*1000)
+        else:
+            pass
+            # error msg
+    
+    elif 'rw' in command:
+        x = re.match(r'^rw( \d+)?$', command)
+        if x:
+            delta = 10
+            if x.group(1) is not None:
+                delta = int(x.group(1).strip())
+
+            player.set_time(player.get_time() - delta*1000)
+        else:
+            pass
+            # error
+    
+    elif 'seek' in command:
+        x = re.match(r'^seek (\d+)%?$', command)
+        if x:
+            l = len(command)
+            if command[l-1] == '%':
+                perc = int(x.group(1))/100
+                player.set_position(perc)
+            else:
+                t = int(x.group(1))
+                player.set_time(t*1000)
+        else:
+            pass
+            #error
+    
+            
+
